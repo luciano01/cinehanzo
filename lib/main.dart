9c1800939597503261f02e7b3f932350e9f1fd4d
Luciano <luciano01.dev@gmail.com>
2024-08-01 16:19:31 -0300

    Setting up Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
@@ -7,6 +8,7 @@ import 'app_widget.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await AppSharedPreferences.init();
   return runApp(
     ModularApp(
